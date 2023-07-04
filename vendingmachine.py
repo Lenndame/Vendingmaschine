01059bed3d79f81dbae5e9833f986a1469f8839e
Lenndame <138380100+Lenndame@users.noreply.github.com>
2023-07-05 01:17:04 +0200

    Update vendingmachine.py
    zusätzliche Optionen eingefügt
    
    immernoch probleme bei ausführung

--- vendingmachine.py
+++ vendingmachine.py
@@ -1,79 +1,151 @@
 import random
 # ---------------------------------------------------------
 # Vending maschine
-# Log: "drink:1 - input_value: 200 - change: 50 - coins: 3 - OK"
 # ---------------------------------------------------------
 
-drinks = {"Coca Cola": 150,
-        "Pepsi Cola": 200,
-        "Sprite":200,
+drinks = {
+    "Coca Cola": 150,
+    "Pepsi Cola": 200,
+    "Sprite": 200,
 }
 
 money = [200, 100, 50]
 coins = []
 paid = []
-log = []
-choice = []
-collected = 0
+stolen = 0
+running = True
 coin_count = 0
-running = 1
+log = {
+    "Total Coins": 0,
+    "Total Paid": 0,
+    "Choice Count": {drink: 0 for drink in drinks},
+    "Amount Stolen": 0,
+    "Swallowed Coins": 0,
+    "You chose violence": 0,
+    "Peaceful": 0,
+}
 
 while running:
-    print("*"*50)
-    for drink in drinks:
-        print(f"Auswahlmöglichkeiten{drink,drinks[drink]/100}")
-    print("*"*50)
-    choice = input("Bitte wählen sie ihr Getränk (q für Abbruch, Log für log): ")
+    print("*" * 50)
+    print("Ihre Auswahlmöglichkeiten sind: ")
+    for drink, price in drinks.items():
+        print(f"{drink}, Preis: {price / 100}€")
+    print("*" * 50)
+    choice = input("Bitte wählen Sie Ihr Getränk (q für Abbruch, Log für Log): ")
+    #log["Choice Count"[drink[choice]]] += 1
+    
     if choice in drinks:
-        print(f"{choice} wurde gewählt! Der Preis ist {drinks[drink]/100}€")
-        log.append(choice)
-        coins = input("Bitte die münzen einzeln einwerfen! ").split(",")
+        print(f"{choice} wurde gewählt! Der Preis beträgt {drinks[choice] / 100}€")
+
+        coins = input("Bitte werfen Sie die Münzen einzeln ein! ").split(",")
+        paid.clear()
+        sad = random.randint(0, 10)
+
         for coin in coins:
+            log["Total Coins"] += 1
+            coin = int(coin)
             coin_count += 1
-            log.append(coin_count)
-            if int(coin) not in money:
-                collected += int(coin)
-                log.append(choice)
-            elif coin_count > 3:
-                collected += int(coin)
-                log.append(choice)
-                coin_count - 3
+            if coin_count > 3:
+                stolen += coin
+                log["Swallowed Coins"] += 3
                 break
+            elif coin not in money:
+                stolen += coin
+                log["Swallowed Coins"] += 1
+                continue
+            elif coin == 100:
+                
+                if sad >= 3:
+                    stolen += coin
+                    log["Swallowed Coins"] += 1
+
             else:
-                paid.append(int(coin))
+                paid.append(coin)
+                log["Total Paid"] += sum(paid)
+                coin_count = 0
+
         while sum(paid) != drinks[choice]:
             if sum(paid) > drinks[choice]:
-                print(f"Ihr Wechselgeld beträgt: {(drinks[choice] - sum(paid)) /100}€")
-                log.append(paid)
-                break
-            elif sum(paid) < drinks[choice]:
-                print(f"Es fehlt noch: {(drinks[choice] - sum(paid)) /100} €")
-                log.append(paid)
-                coins = input("Bitte nachwerfen! ").split(",")
-                for coin in coins:
-                    coin_count += 1
-                    log.append(coin_count)
-                    if coin_count > 3:
-                        collected += int(coin)
-                        log.append(choice)
-                        coin_count - 3
-                        break
-                    elif int(coin) not in money:
-                        collected += int(coin)
-                        log.append(choice)
-                    #elif int(coin) == 100:
-                    #    random.choice
+                print(f"Ihr Wechselgeld beträgt: {(sum(paid) - drinks[choice]) / 100}€")
+                print(f"KLONK! Bitte Ihre {choice} ")
+                coin_count = 0
+                unlucky = random.randint(0, 10)
+                if unlucky >= 8:
+                    print(f"Verdammt, es scheint als ob die {choice} im Schacht stecken geblieben ist!")
+                    stuck = input("Treten, anrempeln oder weggehen? ")
+                    if stuck in ["treten", "anrempeln"]:
+                        log["You chose violence"] += 1
+                        broken = random.randint(0, 10)
+                        if broken >= 6:
+                            print("BRRZZTTT!!! Die Maschine ist Schrott!")
+                        else:
+                            print("Glück gehabt! Das Getränk ist herausgefallen!")
+                    elif stuck == "weggehen":
+                        log["Peaceful"] += 1
+                        lucky = random.randint(0, 10)
+                        if lucky >= 2:
+                            print("Glück gehabt! Das Getränk ist herausgefallen!")
+                        else:
+                            print("Dann bleibst du wohl durstig")
                     else:
-                        paid.append(int(coin))
-                        log.append(paid)
-        else:
-            if sum(paid) == drinks[choice]:
-                print(f"KLONK! bitte Ihre {choice}")
-                log.append(paid)
-                paid.clear
-    elif choice == ("q", "exit", "close"):
-        running - 1
+                        pass
+                break
+
+            while sum(paid) < drinks[choice]:
+                print(f"Es fehlen noch: {(drinks[choice] - sum(paid)) / 100} €")
+                coins = input("Bitte werfen Sie weitere Münzen ein: ").split(",")
+                sad = random.randint(0, 10)
+                for coin in coins:
+                    log["Total Coins"] += 1
+                    coin = int(coin)
+                    coin_count += 1                   
+                    if coin not in money:
+                        stolen += coin
+                        log["Swallowed Coins"] += 1
+                        continue
+                    elif coin == 100:
+                        if sad >= 3:
+                            stolen += coin
+                            log["Swallowed Coins"] += 1
+                    elif coin_count > 3:
+                        stolen += coin
+                        log["Swallowed Coins"] += 3
+                        print("ERROR! Zu viele münzen, upps!")
+                        break
+                    else:
+                        paid.append(coin)
+                        log["Total Paid"] += sum(paid)
+                        coin_count = 0
+
+            else:
+                print(f"KLONK! Bitte Ihre {choice} ")
+                coin_count = 0
+                unlucky = random.randint(0, 10)
+                if unlucky >= 8:
+                    print(f"Verdammt, es scheint als ob die {choice} im Schacht stecken geblieben ist!")
+                    stuck = input("Treten, anrempeln oder weggehen? ")
+                    if stuck in ["treten", "anrempeln"]:
+                        log["You chose violence"] += 1
+                        broken = random.randint(0, 10)
+                        if broken >= 6:
+                            print("BRRZZTTT!!! Die Maschine ist Schrott!")
+                            running = False
+                        else:
+                            print("Glück gehabt! Das Getränk ist herausgefallen!")
+                    elif stuck == "weggehen":
+                        log["Peaceful"] += 1
+                        lucky = random.randint(0, 10)
+                        if lucky >= 2:
+                            print("Glück gehabt! Das Getränk ist herausgefallen!")
+                        else:
+                            print("Dann bleibst du wohl durstig")
+                    else:
+                        pass
+    elif choice == "q":
+        print("Kein Durst")
+        running = False
     elif choice == "Log":
+        log["Amount Stolen"] = stolen
         print(log)
-    # else:
-    #     print("Computer sagt nein!")
+    else:
+        print("Computer sagt nein!")
